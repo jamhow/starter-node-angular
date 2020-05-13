cd4a49dbfc54f319f98abe820e2ec17428f8c71c
James Howard <jamhow444@gmail.com>
2020-05-13 10:37:13 -0700

    made change to ssh host

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.207.106.43',
+      host: '3.81.94.89',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
